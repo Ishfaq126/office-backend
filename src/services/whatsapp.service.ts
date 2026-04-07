7968aca3ab335ad175ba401a4fb4c9b6a7a42f2f
Ishfaq126 <ishfaque.bcs18@iba-suk.edu.pk>
2026-04-07 21:55:41 +0500

    Initial commit: Office Task Manager Backend 22222345jfjygjhjhntshtrhrehtrthtrhrdgdsgerhg

--- src/services/whatsapp.service.ts
+++ src/services/whatsapp.service.ts
@@ -6,7 +6,7 @@ import prisma from '../utils/prisma'; // Adjust path based on your folders
 const whatsappClient = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        browserWSEndpoint: 'ws://office-task-manager-browserless.aeju8m.easypanel.host/chromium',
+        browserWSEndpoint: 'wss://office-task-manager-browserless.aeju8m.easypanel.host/chromium',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     }
 });
